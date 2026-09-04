700cef56df33ce7c9cb1cc4f11bfb91572da0784
Paulo Igor de Oliveira Pessoa <127306158+NuclearPhysicist@users.noreply.github.com>
2026-09-04 20:22:33 -0300

    Add materiais.js with properties for Aluminum

---
+++ materiais.js
@@ -0,0 +1,15 @@
+const materiais = {
+  Al: {
+    nome: "Alumínio",
+    simbolo: "Al",
+    tipo: "Sólido",
+
+    // Valor provisório para teste do programa
+    alpha: 23e-6,
+
+    unidadeAlpha: "K⁻¹",
+
+    referencia:
+      "Valor provisório. A referência experimental será cadastrada posteriormente."
+  }
+};
